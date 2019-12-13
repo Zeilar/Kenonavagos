9f4a933cbf271fe341b994c0f141e5bab5ff8b88
Anita <olsson_anita@live.se>
2019-12-13 12:08:30 +0100

    new header added

--- header.php
+++ header.php
@@ -25,19 +25,21 @@ $container = get_theme_mod( 'understrap_container_type' );
 <?php do_action( 'wp_body_open' ); ?>
 <div class="site" id="page">
 
-	<!-- ******************* The Navbar Area ******************* -->
-	<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
-
-		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
-
-		<nav class="navbar navbar-expand-md navbar-dark bg-primary">
-
-		<?php if ( 'container' == $container ) : ?>
+<!-- <?php if ( 'container' == $container ) : ?>
 			<div class="container">
-		<?php endif; ?>
+		<?php endif; ?>  -->
 
-					<!-- Your site title as branding in the menu -->
+		
+					
+
+<div class="container">
+	<div class="row">
+
+
+<div class="col-md-4">
+<!-- Your site title as branding in the menu -->
 					<?php if ( ! has_custom_logo() ) { ?>
+						
 
 						<?php if ( is_front_page() && is_home() ) : ?>
 
@@ -53,22 +55,42 @@ $container = get_theme_mod( 'understrap_container_type' );
 					<?php } else {
 						the_custom_logo();
 					} ?><!-- end custom logo -->
+</div>
+
+<div class="col-md-4">
+<?php get_search_form(); ?>
+<hr>
+<h6><center>Kenonavagos Fashion</center></h6>
+</div>
+<div class="col-md-4">
+
+<button type="button" class="btn btn-primary pull-right">Login</button>
+</div>
+
+
+	</div>
+</div>
+	<!-- ******************* The Navbar Area ******************* -->
+	<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
+
+		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
+
+		<nav class="navbar navbar-expand-md navbar-dark bg-primary">
 
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
 					<span class="navbar-toggler-icon"></span>
 				</button>
-
+					
 				<!-- The WordPress Menu goes here -->
-				<?php wp_nav_menu(
-					array(
-						'theme_location'  => 'primary',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav ml-auto',
-						'fallback_cb'     => '',
-						'menu_id'         => 'main-menu',
-						'depth'           => 2,
-						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+				<?php wp_nav_menu( array(
+	'theme_location'  => 'primary',
+	'depth'	          => 2, // 1 = no dropdowns, 2 = with dropdowns.
+	'container'       => 'div',
+	'container_class' => 'collapse navbar-collapse',
+	'container_id'    => 'bs-example-navbar-collapse-1',
+	'menu_class'      => 'navbar-nav mr-auto',
+	'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+	'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
 					)
 				); ?>
 			<?php if ( 'container' == $container ) : ?>
