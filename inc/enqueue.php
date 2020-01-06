363a184bf662f74d659ae78db855de56913c023d
Philip <philip_angelin@hotmail.com>
2020-01-06 17:06:43 +0100

    Kommentar

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -11,6 +11,7 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 	 * Load theme's JavaScript and CSS sources.
 	 */
 	function understrap_scripts() {
+		// Initalize the map
 		kn_map();
 
 		// Get the theme data.
